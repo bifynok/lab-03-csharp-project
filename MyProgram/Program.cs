b1a4dceddedffe321d6b8edefb4b009c5b5d0393
Bogdan Kozak <bkozak04@gmail.com>
2024-04-09 12:48:40 +0300

    Made little program on shapp, and added a Dockerfile

---
+++ MyProgram/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace MyOwnProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person
+            {
+                FirstName = "Bogdan",
+                LastName = "Kozak",
+                Age = 19
+            };
+
+            string json = JsonConvert.SerializeObject(person);
+
+            Console.WriteLine("Serialized JSON:");
+            Console.WriteLine(json);
+
+            Person deserializedPerson = JsonConvert.DeserializeObject<Person>(json);
+
+            Console.WriteLine("\nDeserialized Person:");
+            Console.WriteLine($"First Name: {deserializedPerson.FirstName}");
+            Console.WriteLine($"Last Name: {deserializedPerson.LastName}");
+            Console.WriteLine($"Age: {deserializedPerson.Age}");
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # lab-03-csharp-project
+
+This program is only created for the example of creating a docker container. You don't need to compile it, but if you want...
+
+## How to run it?
+
+Download code, after that open terminal in the derictory "/MyProgram" and enter:
+
+`dotnet restore`
+
+After that you need to open MyProgram.csproj file and change one thing:
+
+`<TargetFramework>netcoreapp3.1</TargetFramework>` -> `<TargetFramework>net8.0</TargetFramework>`
+
+Now you can close it and run it with command:
+
+`dotnet run MyProgram.csproj`
+
+## What does it do?
+
+Nothing usefull. It just showing some info in JSON format.
